7be30259b0552d82a2c184728190fcc5f6b799d1
0x332 <kinspotting@protonmail.com>
2019-09-11 18:32:48 +0800

    add: tests

--- test/app.js
+++ test/app.js
@@ -5,18 +5,18 @@ const { getEventArgument } = require('@aragon/test-helpers/events')
 const { hash } = require('eth-ens-namehash')
 const deployDAO = require('./helpers/deployDAO')
 
-const CounterApp = artifacts.require('CounterApp.sol')
+const HCVoting = artifacts.require('HCVoting.sol')
 
 const ANY_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff'
 
-contract('CounterApp', ([appManager, user]) => {
+contract('HCVoting', ([appManager, user]) => {
   let app
 
   beforeEach('deploy dao and app', async () => {
     const { dao, acl } = await deployDAO(appManager)
 
     // Deploy the app's base contract.
-    const appBase = await CounterApp.new()
+    const appBase = await HCVoting.new()
 
     // Instantiate a proxy for the app, using the base contract as its logic implementation.
     const instanceReceipt = await dao.newAppInstance(
@@ -26,35 +26,30 @@ contract('CounterApp', ([appManager, user]) => {
       false, // setDefault - Whether the app proxy is the default proxy.
       { from: appManager }
     )
-    app = CounterApp.at(
+    app = HCVoting.at(
       getEventArgument(instanceReceipt, 'NewAppProxy', 'proxy')
     )
 
     // Set up the app's permissions.
-    await acl.createPermission(
-      ANY_ADDRESS, // entity (who?) - The entity or address that will have the permission.
-      app.address, // app (where?) - The app that holds the role involved in this permission.
-      await app.INCREMENT_ROLE(), // role (what?) - The particular role that the entity is being assigned to in this permission.
-      appManager, // manager - Can grant/revoke further permissions for this role.
-      { from: appManager }
-    )
-    await acl.createPermission(
-      ANY_ADDRESS,
-      app.address,
-      await app.DECREMENT_ROLE(),
-      appManager,
-      { from: appManager }
-    )
+    // await acl.createPermission(
+    //   ANY_ADDRESS, // entity (who?) - The entity or address that will have the permission.
+    //   app.address, // app (where?) - The app that holds the role involved in this permission.
+    //   // await app.INCREMENT_ROLE(), // role (what?) - The particular role that the entity is being assigned to in this permission.
+    //   appManager, // manager - Can grant/revoke further permissions for this role.
+    //   { from: appManager }
+    // )
+    // await acl.createPermission(
+    //   ANY_ADDRESS,
+    //   app.address,
+    //   await app.DECREMENT_ROLE(),
+    //   appManager,
+    //   { from: appManager }
+    // )
 
     await app.initialize()
   })
 
-  it('should be incremented by any address', async () => {
-    await app.increment(1, { from: user })
-    assert.equal(await app.value(), 1)
-  })
-
-  it('should not be decremented if already 0', async () => {
-    await assertRevert(app.decrement(1), 'MATH_SUB_UNDERFLOW')
+  it('dummy', async () => {
+  // Test me!
   })
 })
